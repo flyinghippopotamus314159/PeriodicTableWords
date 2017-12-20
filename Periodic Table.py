ea3da09def1be14be9cdaf5ef3f7605e22e229e0
flyinghippopotamus314159 <h1409637@trbvm.com>
2017-12-20 11:03:12 +0000

    Update Periodic Table.py

--- Periodic Table.py
+++ Periodic Table.py
@@ -1,1 +1,24 @@
-periodicTableSymbols=[["H","B","C","N","O","F","P","S","K","V","Y","I","W","U"],["He","Li","Be","Ne","Na","Mg","Al","Si","Cl","Ar","Ca","Sc","Ti","Cr","Mn","Fe","Co","Ni","Cu","Zn","Ga","Ge","As","Se","Br","Kr","Rb","Sr","Zr","Nb","Mo","Tc","Ru","Rh","Pd","Ag","Cd","In","Sn","Sb","Te","Xe","Cs","Ba","La","Ce","Pr","Nd","Pm","Sm","Eu","Gd","Tb","Dy","Ho","Er","Tm","Yb","Lu","Hf","Ta","Re","Os","Ir","Pt","Au","Hg","Ti","Pb","Bi","Po","At","Rn","Fr","Ra","Ac","Th","Pa","Np","Pu","Am","Cm","Bk","Cf","Es","Fm","Md","No","Lr","Rf","Db","Sg","Bh","Hs","Mt","Ds","Rg","Cn","Nh","Fl","Mc","Lv","Ts","Og"]#chemical elements in order of number of letters and appearance
+periodicTableSymbols=[["H","B","C","N","O","F","P","S","K","V","Y","I","W","U"],['HE', 'LI', 'BE', 'NE', 'NA', 'MG', 'AL', 'SI', 'CL', 'AR', 'CA', 'SC', 'TI', 'CR', 'MN', 'FE', 'CO', 'NI', 'CU', 'ZN', 'GA', 'GE', 'AS', 'SE', 'BR', 'KR', 'RB', 'SR', 'ZR', 'NB', 'MO', 'TC', 'RU', 'RH', 'PD', 'AG', 'CD', 'IN', 'SN', 'SB', 'TE', 'XE', 'CS', 'BA', 'LA', 'CE', 'PR', 'ND', 'PM', 'SM', 'EU', 'GD', 'TB', 'DY', 'HO', 'ER', 'TM', 'YB', 'LU', 'HF', 'TA', 'RE', 'OS', 'IR', 'PT', 'AU', 'HG', 'TI', 'PB', 'BI', 'PO', 'AT', 'RN', 'FR', 'RA', 'AC', 'TH', 'PA', 'NP', 'PU', 'AM', 'CM', 'BK', 'CF', 'ES', 'FM', 'MD', 'NO', 'LR', 'RF', 'DB', 'SG', 'BH', 'HS', 'MT', 'DS', 'RG', 'CN', 'NH', 'FL', 'MC', 'LV', 'TS', 'OG']]#chemical elements in order of number of letters and appearance
+def checkLetter(letter):
+  output=False
+  if len(letter)==1:
+    if letter in periodicTableSymbols[0]:
+      output=letter
+  else:
+    if letter in periodicTableSymbols[1]:
+      output=str(letter[0]+letter[1].lower())
+  return(output)
+def getResult(word):
+  finalOutput=""
+  if word=="":
+    return("\n")
+  elif checkLetter(word[0])!=False:
+    return(getResult(word[1:]))
+  else:
+    return(False)
+while True:
+  word=input("Please enter a word")
+  word=word.upper()
+  getResult(word)
+  
+  
